fa2b784ff1ed146934ab757113ac2bb43446204d
Leyre <55292275+leyrec@users.noreply.github.com>
2020-02-22 22:58:24 -0500

    Add files via upload

---
+++ data/data1.js
@@ -0,0 +1,22 @@
+var data1 = {
+    year_2014: {
+        heart_disease: 1540,
+        malignant_neoplasm: 443,
+        diabetes: 83,
+        flue: 61
+    },
+
+    year_2013: {
+        heart_disease: 1250,
+        malignant_neoplasm: 620,
+        diabetes: 57,
+        flue: 25
+    },
+
+    year_2012: {
+        heart_disease: 1123,
+        malignant_neoplasm: 389,
+        diabetes: 68,
+        flue: 30
+    }
+};
--- plots.js
+++ plots.js
@@ -42,11 +42,67 @@ d3.json("data/data.json").then((importedData) => {
   // Render the plot to the div tag with id "plot"
   Plotly.newPlot("plot_1", chartData, layout);
 
-
-
-
 });
 
+//PieChart
+
+// Create an array of each country's numbers object.values is a method and is important
+var year_2014 = Object.values(data1.year_2014);
+var year_2013 = Object.values(data1.year_2013);
+var year_2012 = Object.values(data1.year_2012);
+
+// Create an array of music provider labels
+var labels = Object.keys(data1.year_2014);
+
+// Display the default plot
+function init() {
+  var data1 = [{
+    values: year_2014,
+    labels: labels,
+    type: "pie"
+  }];
+
+  var layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.newPlot("pie", data1, layout);
+}
+
+// On change to the DOM, call getData()
+//select everything with id selDatasernand when you detect someone messing around (on change) run getData
+
+
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable(if I select US the dataset will correspond to US values)
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data1 = [];
+
+  if (dataset == 'year_2014') {
+      data1 = year_2014;
+  }
+  else if (dataset == 'year_2013') {
+      data1 = year_2013;
+  }
+  else if (dataset == 'year_2012') {
+      data1 = year_2012;
+  }
+  // Call function to update the chart
+  updatePlotly(data1);
+}
+
+// Update the restyled plot's values has to be an array  []of objects otherwise is gonna crash
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
 
 
 
